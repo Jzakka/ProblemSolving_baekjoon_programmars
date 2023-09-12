94cc6fcc7e6ac77adc82e7cec1374de722b9da7c
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-09-12 22:00:11 +0900

    [Bronze I] Title: Integer Square, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/20240. Integer Square/Integer Square.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringBuilder res = new StringBuilder();
+
+    private static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws Exception {
+        int s = Integer.parseInt(br.readLine());
+
+        solution(s);
+
+        printRes();
+    }
+
+    private static void solution(int s) {
+        for (int i = 0; i <= 40; i++) {
+            double k = Math.sqrt(s - i * i);
+            if (k - (int) k == 0) {
+                int j = (int)k;
+                res
+                        .append(i).append(" ").append(0).append("\n")
+                        .append(i + j).append(" ").append(i).append("\n")
+                        .append(0).append(" ").append(j).append("\n")
+                        .append(j).append(" ").append(i + j).append("\n");
+                return;
+            }
+        }
+
+        res.append("Impossible");
+    }
+
+    public static void printRes() throws IOException {
+        bw.write(res.toString());
+        bw.close();
+    }
+}
+
+/*
+n=10
+9876543210
+
+n=11
+89876543210
+98765432101
+10123456789
+
+n=12
+989876543210
+789876543210
+898765432101
+
+898765432101
+987654321010
+987654321012
+
+210123456789
+010123456789
+101234567898
+
+n=13
+8989876543210
+9898765432101
+
+8789876543210
+6789876543210
+7898765432101
+
+9[]0 => 8[]0, 9[]1
+b[]0 => a[]0, c[]0, b[]1
+0[]b => 1[]b, 1[]
+ */
