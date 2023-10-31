310cb81ec476d06ce742b1deb0c21275fdd26a68
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-10-31 18:51:43 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 256 ms, Memory: 19120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,61 +1,68 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.*;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static StringBuilder res = new StringBuilder();
-
+    private static final int MOD = 1_000_000_009;
 
     public static void main(String[] args) throws Exception {
-        int n = Integer.parseInt(br.readLine());
+        br.readLine();
 
-        int[] sequence = Arrays.stream(br.readLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] seq = getInts();
 
-        solution(sequence);
-
+        solution(seq);
         printRes();
     }
 
-    private static void solution(int[] sequence) {
-        List<Integer> vector = new ArrayList<>();
+    private static void solution(int[] seq) {
+        List<Integer> lis = new ArrayList<>();
 
-        for (int number : sequence) {
-            if (!vector.isEmpty() && vector.get(vector.size() - 1) >= number) {
-                int lowerBoundIdx = lowerBound(vector, number);
-                vector.set(lowerBoundIdx, number);
+        for (int num : seq) {
+            if (!lis.isEmpty() && lis.get(lis.size() - 1) >= num) {
+                int pos = upperBound(lis, num);
+                lis.set(pos, num);
             } else {
-                vector.add(number);
+                lis.add(num);
             }
         }
 
-        res.append(vector.size());
+        res.append(lis.size());
     }
 
+    private static int upperBound(List<Integer> lis, int num) {
+        int lo = 0;
+        int hi = lis.size() - 1;
 
-    public static void printRes() throws IOException {
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (lis.get(mid) < num) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+
+    private static int[] getInts() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static long[] getLongs() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToLong(Long::parseLong)
+                .toArray();
+    }
+
+    static void printRes() throws IOException {
         bw.write(res.toString());
         bw.close();
-    }
-
-    private static int lowerBound(List<Integer> arr, int num){
-        int left,right;
-        left = 0;
-        right = arr.size();
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr.get(mid) < num) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        return right;
     }
 }
