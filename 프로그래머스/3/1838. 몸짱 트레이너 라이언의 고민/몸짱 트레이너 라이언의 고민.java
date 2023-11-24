fbfea08d1f29931cdb2e9faf54336a0ed4e02eb4
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-11-24 22:34:02 +0900

    [level 3] Title: 몸짱 트레이너 라이언의 고민, Time: 23.90 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
+++ 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
@@ -1,110 +1,92 @@
 import java.util.*;
 
 class Solution {
-    class Pos{
-        int x,y;
-        Pos(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+    int n;
 
-        int distance(Pos p){
-            return Math.abs(x-p.x) + Math.abs(y-p.y);
-        }
-
-        @Override
-        public boolean equals(Object o){
-            Pos p = (Pos)o;
-            return x==p.x && y==p.y;
-        }
-    }
-    int n,m;
-    int totalLocker;
     public int solution(int n, int m, int[][] timetable) {
         this.n = n;
-        this.m = m;
-        this.totalLocker = n*n;
 
-        Arrays.sort(timetable, (t1,t2)->{
-           if(t1[0] == t2[0]) {
-               return t1[1] - t2[1];
-           }
+        Arrays.sort(timetable, (t1, t2) -> {
+            if (t1[0] == t2[0]) {
+                return t1[1] - t2[1];
+            }
             return t1[0] - t2[0];
         });
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>((t1,t2)->{
-            if(t1[1] == t2[1]) {
-               return t1[0] - t2[0];
-            }
-            return t1[1] - t2[1];
-        });
 
-        int maxPeople = 0;
-        for(int[] time:timetable){
-            if(heap.isEmpty()){
-               heap.add(time);
-            }else{
-                while(!heap.isEmpty() && heap.peek()[1] < time[0]){
-                    heap.poll();
-                }
-                heap.add(time);
-            }
-            maxPeople = Math.max(maxPeople, heap.size());
-        }
+        int maxPerson = getMaxPerson(timetable);
 
-        return farDist(maxPeople);
-    }
-
-    int farDist(int people){
-        if(people == 1){
+        int totalLockers = n * n;
+        if (maxPerson == 1) {
             return 0;
         }
-        // 체스판으로 할당 된 경우 무조건 거리는 2
-        if(people == (totalLocker+1)/2){
+        if (maxPerson == (totalLockers + 1) / 2) {
             return 2;
         }
-        // 체스판보다 할당량이 많으면 거리는 무조건 1
-        if(people > (totalLocker+1)/2){
+        if (maxPerson > (totalLockers + 1) / 2) {
             return 1;
         }
 
-        // 최대거리에서 거리를 줄여나가면서 최대 할당 락커수가 인원수랑 같으면 거리 반환
-        for(int dist = 2*(n-1);dist>=1;dist--){
-            int maxAllocate = allocate(dist);
-            if(maxAllocate >= people){
-                return dist;
+        int lo = 1;
+        int hi = (n - 1) * 2;
+        for (int i = hi; i >= lo; i--) {
+            if (check(i, maxPerson)) {
+                return i;
             }
         }
         return -1;
     }
 
-    int allocate(int dist){
-        int maxCnt = 0;
+    private boolean check(int distance, int maxPerson) {
+        int maxAllocated = 0;
+        for (int k = 0; k < n; k++) {
+            List<int[]> allocated = new ArrayList<>();
+            allocated.add(new int[]{0, k});
 
-        // 처음 할당할 맨 윗 행의 락커의 시작점
-        for(int ty = 0;ty<n;ty++){
-            List<Pos> positions = new ArrayList(Arrays.asList(new Pos(0,ty)));
-
-            for(int i = 0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    Pos pos = new Pos(i,j);
-                    if(further(positions, pos, dist)){
-                        positions.add(pos);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (allocatable(distance, allocated, i, j)) {
+                        allocated.add(new int[]{i, j});
                     }
                 }
             }
-
-            maxCnt = Math.max(maxCnt, positions.size());
+            maxAllocated = Math.max(maxAllocated, allocated.size());
         }
 
-        return maxCnt;
+        return maxAllocated >= maxPerson;
     }
 
-    boolean further(List<Pos> positions, Pos pos, int dist){
-        for(Pos allocated:positions){
-            if(allocated.distance(pos) < dist){
+    private boolean allocatable(int distance, List<int[]> allocated, int x, int y) {
+        for (int[] locker : allocated) {
+            if (getDistance(locker[0], locker[1], x, y) < distance) {
                 return false;
             }
         }
         return true;
+    }
+
+    private int getDistance(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    private int getMaxPerson(int[][] timetable) {
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+        int person = 0;
+
+        for (int i = 0; i < timetable.length; i++) {
+            int[] time = timetable[i];
+
+            while (!PQ.isEmpty() && PQ.peek()[1] < time[0]) {
+                PQ.poll();
+            }
+            PQ.add(time);
+            person = Math.max(person, PQ.size());
+        }
+
+        return person;
     }
 }
