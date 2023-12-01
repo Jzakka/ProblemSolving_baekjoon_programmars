3d3fda178ebe04d442f198089d9060dd911162cb
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-12-01 18:24:16 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 1948 ms, Memory: 159232 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringBuilder res = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] times = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            times[i] = getInts();
+        }
+
+        solution(times);
+
+        printRes();
+    }
+
+    private static void solution(int[][] times) {
+        Arrays.sort(times, (r1, r2)->{
+            if (r1[0] == r2[0]) {
+                return r1[1] - r2[1];
+            }
+            return r1[0] - r2[0];
+        });
+
+        TreeSet<Integer> rooms = new TreeSet<>();
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int[] time : times) {
+            int start = time[0];
+            int end = time[1];
+
+
+            Integer floor;
+            if (rooms.isEmpty() || (floor = rooms.floor(start)) == null) {
+                setAdd(rooms, counts, end);
+            } else{
+                setSub(rooms, counts, floor);
+                setAdd(rooms, counts, end);
+            }
+        }
+
+        int ans = counts.values().stream().mapToInt(Integer::intValue).sum();
+
+        res.append(ans);
+    }
+
+    private static void setAdd(TreeSet<Integer> rooms, Map<Integer, Integer> counts, Integer val) {
+        rooms.add(val);
+        if (!counts.containsKey(val)) {
+            counts.put(val, 1);
+        } else {
+            counts.put(val, counts.get(val) + 1);
+        }
+    }
+
+    private static void setSub(TreeSet<Integer> rooms, Map<Integer, Integer> counts, Integer val) {
+        counts.put(val, counts.get(val) - 1);
+        if (counts.get(val) == 0) {
+            counts.remove(val);
+            rooms.remove(val);
+        }
+    }
+
+    private static int[] getInts() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+    private static long[] getLongs() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToLong(Long::parseLong)
+                .toArray();
+    }
+    public static void printRes() throws IOException {
+        bw.write(res.toString());
+        bw.close();
+    }
+}
