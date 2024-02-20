f6fdc29350ef1bb8939ab6230b9e5123e70901bd
Jzakka <105845911+jzakka@users.noreply.github.com>
2024-02-20 19:26:12 +0900

    [Gold IV] Title: 트리의 지름, Time: 672 ms, Memory: 44444 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringBuilder res = new StringBuilder();
+
+    private static int[] dx = {-1, 0, 0, 1};
+    private static int[] dy = {0, -1, 1, 0};
+
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        List<int[]>[] graph = IntStream.rangeClosed(0, n)
+                .mapToObj(i -> new ArrayList())
+                .toArray(List[]::new);
+        for (int i = 0; i < n-1; i++) {
+            int[] edge = getInts();
+
+            int src = edge[0];
+            int dest = edge[1];
+            int cost = edge[2];
+
+            graph[src].add(new int[]{dest, cost});
+            graph[dest].add(new int[]{src, cost});
+        }
+        solution(graph);
+
+        printRes();
+    }
+
+    static class IntRef {
+        int val = 0;
+    }
+
+    private static void solution(List<int[]>[] graph) {
+        IntRef ans = new IntRef();
+
+        traversal(graph, new HashSet<>(), 1, ans);
+
+        res.append(ans.val);
+    }
+
+    /**
+     * @param graph
+     * @param visited
+     * @param cur
+     * @param ans
+     * @return 자식가는 cost + 자식 반환값 중 젤루 큰놈
+     */
+    private static int traversal(List<int[]>[] graph, Set<Integer> visited, int cur, IntRef ans) {
+        visited.add(cur);
+
+        List<Integer> childsRes = new ArrayList<>();
+        for (int[] incident : graph[cur]) {
+            int child = incident[0];
+            int cost = incident[1];
+
+            if (!visited.contains(child)) {
+                int childRes = traversal(graph, visited, child, ans) + cost;
+                childsRes.add(childRes);
+            }
+        }
+        childsRes.sort(Comparator.comparingInt(Integer::intValue).reversed());
+
+        int childLinkLength = 0;
+        for (int i = 0; i < Math.min(2, childsRes.size()); i++) {
+            childLinkLength += childsRes.get(i);
+        }
+        ans.val = Math.max(ans.val, childLinkLength);
+
+        return childsRes.stream().findFirst().orElse(0);
+    }
+
+    private static int[] getInts() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static long[] getLongs() throws IOException {
+        return Arrays.stream(br.readLine().split("\\s+"))
+                .mapToLong(Long::parseLong)
+                .toArray();
+    }
+
+    public static void printRes() throws IOException {
+        bw.write(res.toString());
+        bw.close();
+    }
+}
+
+/*
+9
+1 2 1 7 2 -1
+2 1 1 3 30 4 22 -1
+3 2 30 -1
+4 2 22 5 7 6 10 -1
+5 4 7 -1
+6 4 10 -1
+7 1 2 9 3 8 5 -1
+8 7 5 -1
+9 7 3 -1
+ */
