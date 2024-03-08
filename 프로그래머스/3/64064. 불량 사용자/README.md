4321f0c613d1f7ddd2c2ea760b4be953b881ebee
Jzakka <105845911+jzakka@users.noreply.github.com>
2024-03-08 23:15:39 +0900

    [level 3] Title: 불량 사용자, Time: 3.14 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064#) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.43 ms
+메모리: 75.7 MB, 시간: 3.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 15:45:45
+2024년 03월 08일 23:15:37
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,47 +1,39 @@
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static java.lang.System.*;
 
 class Solution {
-    Set<Set<Integer>> ans = new HashSet<>();
+    Set<Set<Integer>> combinations = new HashSet();
+    
     public int solution(String[] user_id, String[] banned_id) {
-        List<Integer> combination = new ArrayList<>();
-        combinate(user_id, banned_id, 0, combination);
-
-        return ans.size();
+        // out.println("frodo".matches("fr.d."));
+        for(int i=0;i<banned_id.length;i++){
+            banned_id[i] = banned_id[i].replaceAll("\\*", ".");
+        }
+        
+        count(user_id,banned_id, 0, new HashSet());
+        return combinations.size();
     }
-
-    private void combinate(String[] users, String[] bans, int banIdx, List<Integer> combination) {
-        if (banIdx == bans.length) {
-            ans.add(new HashSet<>(combination));
+    
+    void count(String[] users, String[] banned, int i, Set<Integer> combination){
+        if(i == banned.length){
+            combinations.add(new HashSet(combination));
             return;
         }
-
-        for (int i = 0; i < users.length; i++) {
-            String user = users[i];
-
-            if (!combination.contains(i) && match(user, bans[banIdx])) {
-                combination.add(i);
-                combinate(users, bans, banIdx + 1, combination);
-                combination.remove((Integer) i);
+        
+        for(int j = 0;j<users.length;j++){
+            if(users[j].matches(banned[i]) && !combination.contains(j)){
+                combination.add(j);
+                count(users, banned, i+1, combination);
+                combination.remove(j);
             }
         }
     }
-
-    private boolean match(String user, String ban) {
-        int len = user.length();
-        if (len != ban.length()) {
-            return false;
+    
+    void debug(int i, int j){
+        for(int t=0;t<i;t++){
+            out.print("\t");
         }
-
-        for (int i = 0; i < len; i++) {
-            if (ban.charAt(i) == '*') {
-                continue;
-            }
-            if (user.charAt(i) != ban.charAt(i)) {
-                return false;
-            }
-        }
-
-        return true;
+        out.println("i: " + i + " j: " + j);
     }
 }
