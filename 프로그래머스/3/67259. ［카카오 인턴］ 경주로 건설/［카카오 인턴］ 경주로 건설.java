a8e86c216996ae9886b3a6287a43c2acf160a9b2
Jzakka <105845911+jzakka@users.noreply.github.com>
2024-03-10 14:43:01 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.74 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,70 +1,69 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.lang.System.*;
 
 class Solution {
-    int[] dx = {-1, 0, 0, 1};
-    int[] dy = {0, -1, 1, 0};
+    final int UP = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+    final int DOWN  = 3;
+    
+    final int[] dx = {-1,0,0,1};
+    final int[] dy = {0,-1,1,0};
     int[][][] DP;
+    int n , m;
     public int solution(int[][] board) {
-        int n = board.length;
-        DP = new int[n][n][4];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 4; k++) {
-                    DP[i][j][k] = Integer.MAX_VALUE;
-                }
+        n = board.length;
+        m = board[0].length;
+        // DP[x][y][d] : (x,y)를 d방향에서 진입했을 때 드는 최소 비용
+        DP = new int[n][m][4];
+        Arrays.stream(DP)
+            .forEach(mat -> Arrays.stream(mat).forEach(row -> Arrays.fill(row, Integer.MAX_VALUE)));
+        DP[0][0][DOWN] = DP[0][0][RIGHT] = 0;
+        
+        dfs(board, 0,0,DOWN);
+        dfs(board, 0,0,RIGHT);
+        // debug();
+        return Math.min(DP[n-1][m-1][DOWN], DP[n-1][m-1][RIGHT]);
+    }
+    
+    void dfs(int[][] board, int x,int y, int curDir){
+        
+        for(int dir=0;dir<4;dir++){
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            
+            int cost = getCost(curDir, dir);
+            if(dir + curDir != 3 && inRange(nx,ny) && board[nx][ny] == 0 && DP[nx][ny][dir] > DP[x][y][curDir] + cost){
+                DP[nx][ny][dir] = DP[x][y][curDir] + cost;
+                dfs(board, nx, ny, dir);
             }
         }
-
-        dijkstra(board);
-
-        return Math.min(DP[n - 1][n - 1][2], DP[n - 1][n - 1][3]);
     }
-
-    private void dijkstra(int[][] board) {
-        DP[0][0][0] =DP[0][0][1] =DP[0][0][2] =DP[0][0][3] =0;
-        PriorityQueue<int[]> PQ = new PriorityQueue<>(Comparator.comparingInt(a -> DP[a[0]][a[1]][a[2]]));
-        PQ.add(new int[]{0, 0, 2, 0});
-        PQ.add(new int[]{0, 0, 3, 0});
-
-        while (!PQ.isEmpty()) {
-            int[] cur = PQ.poll();
-
-            int x = cur[0];
-            int y = cur[1];
-            int vector = cur[2];
-            int cost = cur[3];
-
-//            System.out.println("x = " + x);
-//            System.out.println("y = " + y);
-//            System.out.println("vector = " + vector);
-//            System.out.println();
-
-            if (cost > DP[x][y][vector]) {
-                continue;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                int movedCost;
-                if (available(nx, ny, board) && DP[nx][ny][i] > (movedCost = addCost(x, y, vector, i))) {
-                    DP[nx][ny][i] = movedCost;
-                    PQ.add(new int[]{nx, ny, i, movedCost});
-                }
-            }
+    
+    int getCost(int dir1, int dir2){
+        if(dir1 == dir2){
+            return 100;
         }
-
+        return 600;
     }
-
-    private int addCost(int x, int y, int vector, int nextVector) {
-        return DP[x][y][vector] + (vector == nextVector ? 100 : 600);
+    
+    boolean inRange(int x, int y){
+        return 0<=x && x<n && 0<=y&&y<m;
     }
-
-    private boolean available(int x, int y, int[][] board) {
-        int n = board.length;
-
-        return 0 <= x && x < n && 0 <= y && y < n && board[x][y] == 0;
+    
+    void debug(){
+        out.println("===DEBUG===");
+        for(int[][] mat:DP){
+            for(int[] row:mat){
+                out.print("[");
+                for(int e: row){
+                    out.print(e + ",");
+                }
+                out.print("]");
+            }
+            out.println();
+        }
     }
 }
