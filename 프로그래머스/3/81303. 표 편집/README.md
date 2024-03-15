553190b126eada208f9b1828867806380447894b
Jzakka <105845911+jzakka@users.noreply.github.com>
2024-03-15 18:25:45 +0900

    [level 3] Title: 표 편집, Time: 444.53 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/README.md
+++ 프로그래머스/3/81303. 표 편집/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표 편집 - 81303 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303) 
 
 ### 성능 요약
 
-메모리: 194 MB, 시간: 885.43 ms
+메모리: 173 MB, 시간: 444.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 13:4:26
+2024년 03월 15일 18:25:43
 
 ### 문제 설명
 
--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,69 +1,74 @@
-import java.util.Spliterator;
-import java.util.Stack;
-import java.util.TreeSet;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.lang.System.*;
 
 class Solution {
-    Stack<Integer> history = new Stack<>();
-    TreeSet<Integer> table = new TreeSet<>();
-    int cursorKey = 0;
-
-    public String solution(int n, int k, String[] cmds) {
-        IntStream.range(0, n).forEach(i -> table.add(i));
-        cursorKey = k;
-
-        for (String cmd : cmds) {
-            char action = cmd.charAt(0);
-
-            if (action == 'U' || action == 'D') {
-                int moveCnt = Integer.parseInt(cmd.substring(2));
-                move(action, moveCnt);
-            } else if (action == 'C') {
-                delete();
-            } else {
-                undo();
+    TreeSet<Integer> table;
+    Deque<Integer> stk = new ArrayDeque();
+    boolean[] deleted;
+    int cursor;
+    public String solution(int n, int k, String[] cmd) {
+        deleted = new boolean[n];
+        table = IntStream.range(0,n).boxed().collect(Collectors.toCollection(TreeSet::new));
+        cursor = k;
+        
+        for(String c:cmd){
+            char action = c.charAt(0);
+            switch(action){
+                case 'U':
+                    go(-Integer.parseInt(c.substring(2)));
+                    break;
+                case 'D':
+                    go(Integer.parseInt(c.substring(2)));
+                    break;
+                case 'C':
+                    delete();
+                    break;
+                case 'Z':
+                    undo();
+                    break;
             }
         }
-
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (table.contains(i)) {
-                res.append("O");
-            } else {
-                res.append("X");
+        
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<n;i++){
+            ans.append(deleted[i] ? "X" : "O");
+        }
+        return ans.toString();
+    }
+    
+    void go(int x){
+        Integer next;
+        if(x < 0){
+            x *= -1;
+            while(x > 0 && (next=table.lower(cursor)) != null){
+                cursor = next;
+                x--;
             }
-        }
-        return res.toString();
-    }
-
-    private void undo() {
-        Integer recent = history.pop();
-        table.add(recent);
-    }
-
-    private void delete() {
-        if (table.last() == cursorKey) {
-            Integer last = table.last();
-            cursorKey = table.lower(last);
-            table.remove(last);
-            history.push(last);
-        } else {
-            cursorKey = table.higher(cursorKey);
-            Integer deleted = table.lower(cursorKey);
-            table.remove(deleted);
-            history.push(deleted);
-        }
-    }
-
-    private void move(char direction, int moveCnt) {
-        for (int i = 0; i < moveCnt; i++) {
-            if (direction == 'U') {
-                cursorKey = table.lower(cursorKey);
-            } else {
-                cursorKey = table.higher(cursorKey);
+        }else {
+            while(x > 0 && (next=table.higher(cursor)) != null){
+                cursor = next;
+                x--;
             }
         }
     }
-
-
+    
+    void delete(){
+        int toDelete = cursor;
+        if(cursor == table.last()){
+            cursor = table.lower(cursor);
+        }else{
+            cursor = table.higher(cursor);
+        }
+        table.remove(toDelete);
+        deleted[toDelete] = true;
+        stk.offerLast(toDelete);
+    }
+    
+    void undo(){
+        int toRestore = stk.pollLast();
+        deleted[toRestore] = false;
+        table.add(toRestore);
+    }
 }
